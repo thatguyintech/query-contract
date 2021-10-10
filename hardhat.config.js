dc2a220b9969086b123386c7aa45504bdd144111
Albert Hu <albert@alchemy.com>
2021-10-09 23:10:01 -0700

    query the contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL } = process.env;
+
 module.exports = {
-  solidity: "0.7.3",
-};
+   solidity: "0.7.3",
+   defaultNetwork: "ropsten",
+   networks: {
+      mainnet: {
+         url: API_URL
+      }
+   },
+}
---
+++ interact.js
@@ -0,0 +1,67 @@
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const API_KEY = process.env.API_KEY;
+const { ethers } = require("hardhat");
+
+const contractAbi = JSON.parse(`[
+    {
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "tokenURI",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "baseTokenURI",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]`);
+
+const provider = new ethers.providers.AlchemyProvider(network="mainnet", apiKey = API_KEY);
+const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi, provider);
+
+async function main() {
+    const name= await contract.name();
+    console.log("contract name: ", name);
+
+    const uri = await contract.tokenURI(15);
+    console.log("token uri", uri);
+
+    const baseTokenUri = await contract.baseTokenURI();
+    console.log("baseTokenUri", baseTokenUri);
+}
+
+main();
